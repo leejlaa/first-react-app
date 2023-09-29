b7a0f4bfa2dce6a8240f609d0a557805ac63759b
leejlaa <lejladoric00@gmail.com>
2023-09-29 14:41:36 +0200

    added props and states

--- src/App.js
+++ src/App.js
@@ -1,58 +1,103 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 
-const list = [
-  {
-  title: 'React',
-  url: 'https://reactjs.org/',
-  author: 'Jordan Walke',
-  num_comments: 3,
-  points: 4,
-  objectID: 0,
-  },
-  {
-  title: 'Redux',
-  url: 'https://redux.js.org/',
-  author: 'Dan Abramov, Andrew Clark',
-  num_comments: 2,
-  points: 5,
-  objectID: 1,
-  },
-  ];
 
-function List(){
+
+
+ 
+function Search(){
+
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+    console.log(searchTerm);
+  }
+
+  return(<>
+    <label htmlFor='search'>Search: </label>
+    <input id="search" type="text" onChange={handleChange}></input>
+  </>);
+}
+
+
+// class Person{
+//   constructor(firstName, lastName){
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
+
+//   getName(){
+//     return this.firstName + " " + this.lastName;
+//   }
+// }
+
+// const lejla = new Person("leki", "D.");
+// console.log(lejla.getName);
+
+function Item(props){
+  return(
+    <div>
+      <li>
+                <span>
+                  <a href={props.item.url}>{props.item.title}</a>
+                </span>
+                <span>{props.item.author}</span>
+                <span>{props.item.num_comments}</span>
+                <span>{props.item.points}</span>
+      </li>
+    </div>
+  );
+}
+function List(props){
   return(
     <ul>
       {
-        list.map(function(item){
+        props.list.map(function(item){
             return(
-              <li key={item.objectID}>
-                <span>
-                  <a href={item.url}>{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-              </li>
+              <Item key ={item.objectID} item={item}/>
             );
           }
         )
       }
+    <Search/>
     </ul>
+   
   );
 }
-function Header(){
-  return (<>
-    <h1>Hi world</h1>
-    <List/>
-    </>
+// function Header(){
+//   return (<>
+//     <h1>Hi world</h1>
+//     <List/>
+//     </>
 
-  ); // my first functional compoment
-}
+//   ); // my first functional compoment
+// }
 
 function App() {
-  return <Header/>;
+  const stories = [
+    {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+    },
+    {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+    },
+    ];
+  return <div>
+    <List list ={stories}/>
+    </div>;
 }
 
 export default App;
